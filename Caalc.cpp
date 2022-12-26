2aafbc5a3a9f9599c5032247c7a18a8ae20fc421
Toondrus <ochirovvladik@gmail.com>
2022-12-26 13:24:47 +0800

    Output matrix function

--- Caalc.cpp
+++ Caalc.cpp
@@ -7,3 +7,14 @@ void Input_Matrix(int Lines, int Columns, int Matrix[20][20])
         cin >> A[i][j];
     }
 }
+void Output_Matrix(int Lines, int Columns, int Matrix[20][20])
+{
+    for(int i = 0; i < Lines; i++)
+    {
+        for(int j = 0; j < Columns; j++)
+        {
+            cout.width(7); cout << A[i][j];
+        }
+        cout << endl;
+    }
+}
